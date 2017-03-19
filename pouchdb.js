03718a9cded70780714e109a4f57b285887fbaf4
System <none>
2017-03-19 19:32:48 +0000

    Checkpoint

--- pouchdb.js
+++ pouchdb.js
@@ -2,35 +2,24 @@ var PouchDB = require('pouchdb');
 
 var db = new PouchDB('dbname');
 
-db.get('last_time').then(function(doc) {
-  console.log(doc);
-  // return db.put({
-  //   _id: 'mydoc',
-  //   _rev: doc._rev,
-  //   title: "Let's Dance"
-  // });
-}).then(function(response) {
-  // handle response
-  console.log("Response", response);
+var existingDocumentRev;
+
+db.get('last_ran').then(function(document) {
+  existingDocumentRev = document['_rev'];
 }).catch(function (err) {
-  console.log("Error", err);
-  if (err.status == 404){
-    console.log("Not found");
-  }
+  console.log("No last_ran document")
 }).then(function(response){
-  var latestTime = Date.now();
-  console.log(latestTime);
-  return db.put({
-      _id: 'last_time',
-      latestTimetime: latestTime
-    });
-}).catch(function (err) {
-  if (err.name === 'conflict') {
-    console.log("ERROR! CONFLICT!")
-    // conflict!
-  } else {
-    console.log("OTHER ERROR")
-    // some other error
+  var now = Date.now(),
+      lastRanDocument = {
+        _id: 'state',
+        time: now
+      }
+  if (existingDocumentRev){
+    lastRanDocument['_rev'] = existingDocumentRev;
   }
+  
+  return db.put(lastRanDocument);
+}).catch(function (err) {
+  console.log("Failed to update last_ran document");
 });
 
