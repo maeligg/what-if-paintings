ea6683031709640fb3e3aa8bfca55b2bf0ca9fba
System <none>
2017-03-19 19:22:44 +0000

    Checkpoint

--- pouchdb.js
+++ pouchdb.js
@@ -2,7 +2,7 @@ var PouchDB = require('pouchdb');
 
 var db = new PouchDB('dbname');
 
-db.get('mydoc').then(function(doc) {
+db.get('last_time').then(function(doc) {
   console.log(doc);
   // return db.put({
   //   _id: 'mydoc',
@@ -16,9 +16,21 @@ db.get('mydoc').then(function(doc) {
   console.log("Error", err);
   if (err.status == 404){
     console.log("Not found");
-    return db.put({
-      _id: 'mydoc',
-      title: "Let's Dance"
+  }
+}).then(function(response){
+  var latestTime = Date.now();
+  console.log(latestTime);
+  return db.put({
+      _id: 'last_time',
+      latestTimetime: latestTime
     });
+}).catch(function (err) {
+  if (err.name === 'conflict') {
+    console.log("ERROR! CONFLICT!")
+    // conflict!
+  } else {
+    console.log("OTHER ERROR")
+    // some other error
   }
 });
+
