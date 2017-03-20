a91a0b706d44358ff50005ec774b86eb5dc507dd
System <none>
2017-03-20 14:40:20 +0000

    Checkpoint

--- bot.js
+++ bot.js
@@ -1,72 +1,28 @@
 var express = require('express'),
-    storage = require('node-persist'),
-    Twit = require('twit'),
-    grammar = require('./tracery.js').grammar,
-    twit;
+    twitter = require('./twitter.js'),
+    grammar = require('./tracery.js').grammar;
 
-storage.initSync();
-
-var twitConfig = {
-  
-}
 
 function generateStatus() {
   // Generate a new tweet using our grammar
   return grammar.flatten("#origin#"); // make sure an "origin" entry is in your grammar.json file
 }
 
-function postTweet(status){
-  // Post a status to twitter to Twitter
-  twit.post('statuses/update', { status: status }, function(err, data, response) {
-    console.log(`Posted status: ${status}`)
-  });
-}
-
-function tryToTweet(){
-  var now = Date.now(), // time since epoch in millisecond
-      lastRun = storage.getItemSync("lastRun") || 0, // last time we were run in milliseconds
-      postDelay = process.env.POST_DELAY_IN_MINUTES || 60, // time to delay between tweets in minutes
-      status;
-  
-  if (now - lastRun > (1000 * 60 * postDelay)) { //Post every process.env.POST_DELAY_IN_MINUTES or 60 minutes
-    status = generateStatus();
-    if (status.length <= 140){
-      console.log("Tweeting!");
-      postTweet(status);
-      storage.setItemSync("lastRun", now);
-    } else {
-      console.log(`Status too long: ${status}`);
-    }
-  } else {
-    console.log(`It's too soon, we only post every ${postDelay} minutes. It's only been ${ Math.floor((now - lastRun) / 60 / 1000 ) } minutes`);
-  }
-}
 
 var app = express();
 app.use(express.static('public')); // serve static files like index.html
 
-try {
-  twit = new Twit({
-    consumer_key:         process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
-    access_token:         process.env.TWITTER_ACCESS_TOKEN,
-    access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET
-  });
-} catch(err) {
-  console.error(err);
-  console.log("Sorry, your .env file does not have the correct settings in order to tweet");
-}
-
-if (twit){
-  // http://expressjs.com/en/starter/basic-routing.html
-  app.get("/tweet", function (request, response) {
-    console.log("Got a hit!");
-    tryToTweet();
-    response.sendStatus(200)
-  });
-
-  console.log("Ready to tweet!");
-}
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/tweet", function (request, response) {
+  var newStatus = generateStatus();
+  console.log("Got a hit!");
+  if (twitter.tryToTweet(newStatus)){
+    response.sendStatus(200);
+  } else {
+    response.sendStatus(500);
+  }
+  
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
--- twitter.js
+++ twitter.js
@@ -11,54 +11,41 @@ try {
     access_token:         process.env.TWITTER_ACCESS_TOKEN,
     access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET
   });
+  console.log("Ready to tweet!");
 } catch(err) {
   console.error(err);
   console.error("Sorry, your .env file does not have the correct settings in order to tweet");
 }
 
 function postTweet(status){
+  if (twit){
+    console.log("Tweeting!");
+    twit.post('statuses/update', { status: status }, function(err, data, response) {
+      console.log(`Posted status: ${status}`)
+    });
+    return true;
+  } else {
+    console.error("Sorry, have haven't setup twitter yet in your .env")
+    return false;
+  }
   // Post a status to twitter to Twitter
-  twit.post('statuses/update', { status: status }, function(err, data, response) {
-    console.log(`Posted status: ${status}`)
-  });
+  
 }
 
 module.exports.tryToTweet = function(status){
   var now = Date.now(), // time since epoch in millisecond
       lastRun = storage.getItemSync("lastRun") || 0, // last time we were run in milliseconds
-      postDelay = process.env.POST_DELAY_IN_MINUTES || 60, // time to delay between tweets in minutes
-      status;
-  
+      postDelay = process.env.POST_DELAY_IN_MINUTES || 60;// time to delay between tweets in minutes
+
   if (now - lastRun > (1000 * 60 * postDelay)) { //Post every process.env.POST_DELAY_IN_MINUTES or 60 minutes
-    status = generateStatus();
     if (status.length <= 140){
-      console.log("Tweeting!");
-      postTweet(status);
       storage.setItemSync("lastRun", now);
+      return postTweet(status);
     } else {
-      console.log(`Status too long: ${status}`);
+      console.error(`Status too long: ${status}`);
     }
   } else {
-    console.log(`It's too soon, we only post every ${postDelay} minutes. It's only been ${ Math.floor((now - lastRun) / 60 / 1000 ) } minutes`);
+    console.error(`It's too soon, we only post every ${postDelay} minutes. It's only been ${ Math.floor((now - lastRun) / 60 / 1000 ) } minutes`);
   }
+  ret
 }
-
-var app = express();
-app.use(express.static('public')); // serve static files like index.html
-
-
-if (twit){
-  // http://expressjs.com/en/starter/basic-routing.html
-  app.get("/tweet", function (request, response) {
-    console.log("Got a hit!");
-    tryToTweet();
-    response.sendStatus(200)
-  });
-
-  console.log("Ready to tweet!");
-}
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
