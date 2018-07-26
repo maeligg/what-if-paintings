9bc1663aef052bf8457fc44ee5e0d0bae42ea3f8
Glitch (fediverse-bot) <none>
2018-07-26 18:55:10 +0000

    💿🎿 Checkpoint ./watch.json:178972/249 ./.gitignore:178972/7 ./bot.js:178972/1695

--- bot.js
+++ bot.js
@@ -1,32 +1,17 @@
 var express = require('express'),
-    mastodon = require('./mastodon.js'), // this require() will log an error if you don't have your .env file setup correctly
-    grammar = require('./tracery.js').grammar;
+    mastodon = require('./fediverse/mastodon.js');
 
 var app = express();
+app.use(express.static('public'));
 
-app.use(express.static('public')); // serve static files like index.html http://expressjs.com/en/starter/static-files.html
-
-function generateStatus() {
-  // Generate a new tweet using our grammar
-  return grammar.flatten("#origin#"); // make sure an "origin" entry is in your grammar.json file
-}
-
-app.all("/toot", function (request, response) { // send a GET or POST to /toot to trigger a toot http://expressjs.com/en/starter/basic-routing.html
-  var newStatus = generateStatus();
-
-  console.log("Got a hit!");
-  if (mastodon.tryToToot(newStatus)){ // Some things could prevent us from tooting. Find out more in mastodon.js
-    response.sendStatus(200);  // We successfully tweeted
+app.all("/" + process.env.BOT_ENDPOINT, function (req, res) {
+  if (mastodon.tryToToot('Hello world')){
+    res.sendStatus(200);
   } else {
-    response.sendStatus(500); // Something prevented us from toot
+    res.sendStatus(500);
   }
-  
 });
 
-// listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-  console.log('Here are some statuses:');
-  for(var i = 0; i < 5; i++){console.log(generateStatus())};
-  console.log("✨🔮✨")
+  console.log('your bot is running on port ' + listener.address().port);
 });
