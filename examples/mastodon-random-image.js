e9b9a839c3cdc5a34d40ee24e1d6703f77549972
Glitch (fediverse-bot) <none>
2018-08-09 17:48:07 +0000

    🚲🌑 Checkpoint ./examples/mastodon-random-image.js:178972/453

--- examples/mastodon-random-image.js
+++ examples/mastodon-random-image.js
@@ -6,11 +6,13 @@ var express = require('express'),
 app.use(express.static('public'));
 
 app.all(`/${process.env.BOT_ENDPOINT}`, function (req, res) {
-  helpers.load_image_assets(function(err, urls){
-    
-    /* First, load images from the assets folder. */
+  /* First, load images from the assets folder. */
 
-    if (!err && urls && urls.length > 0){
+  helpers.load_image_assets(function(err, urls){
+
+    /* Let's make sure we have some images. */
+
+    if (urls && urls.length > 0){
 
       /* Pick a random image. */
       
@@ -19,7 +21,7 @@ app.all(`/${process.env.BOT_ENDPOINT}`, function (req, res) {
       /* You could also get the first image alphabetically. */
       //  var url = urls.sort()[0];
 
-      /* If you want to delete the image after it's posted, update your .env file: */
+      /* If you want to delete the image after it's posted, add this to your .env file: */
       //  REMOVE_POSTED_IMAGES='yes'
 
       helpers.load_image(url, function(err, img_file){
@@ -28,8 +30,7 @@ app.all(`/${process.env.BOT_ENDPOINT}`, function (req, res) {
           'New picture!'
         ]), img_file, function(err){
           if (!err){
-            var remove_posted_images = process.env.REMOVE_POSTED_IMAGES;
-            if (remove_posted_images === 'yes' || remove_posted_images === 'true'){
+            if (process.env.REMOVE_POSTED_IMAGES === 'yes'){
               helpers.remove_asset(url);
             }
           }        
