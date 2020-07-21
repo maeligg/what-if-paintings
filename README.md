6803e1a37f7f56e40188490dd653c3705e0715d6
Maël Brunet <maelbrunet@gmail.com>
2020-07-21 17:56:13 +0200

    Migrate away from glitch and simplify script

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # What if paintings
 
-This is a Node.js bot that posts images on Mastodon at [@what_if_paintings](https://botsin.space/@what_if_paintings). It is based on the [Fediverse bot starter project](https://glitch.com/edit/#!/fediverse-bot) by [@stefan](https://glitch.com/@stefan). The images are pregenerated using Tensorflow and [neural-style](https://github.com/anishathalye/neural-style).
+This is a Node.js bot that posts images on Mastodon at [@what_if_paintings](https://botsin.space/@what_if_paintings) and on Twitter at [@WhatIfPaintings](https://twitter.com/WhatIfPaintings). The images are pregenerated using Tensorflow and [neural-style](https://github.com/anishathalye/neural-style).
 
 ## Set up
 
-1. Remix this project.
+1. Fork this project.
 2. [Create an account for your bot](https://botwiki.org/resource/tutorial/how-to-make-a-mastodon-botsin-space-app-bot/), preferably on [botsin.space](http://botsin.space), a Mastodon instance dedicated to bots.
 3. Update your `.env` file:
-
 - `MASTODON_API`: for example `https://mastodon.social/api/v1/`, or `https://botsin.space/api/v1/`, based on where you created your bot.
-- `MASTODON_ACCESS_TOKEN`: the token you acquired in step 2
-- `BOT_ENDPOINT`: part of the URL used to wake up your bot (`https://PROJECT_NAME.glitch.me/BOT_ENDPOINT`)
-
-
-4. Update `bot.js` with your bot's code. See the `examples` folder for what you can do. Also check out the [documentation for the node-mastodon library](https://github.com/jhayley/node-mastodon) and the [Mastodon API](https://github.com/tootsuite/documentation/blob/master/Using-the-API/API.md).
+- `MASTODON_ACCESS_TOKEN`: the Mastodon token you acquired in step 2
+- `CONSUMER_KEY`: from the Twitter API
+- `CONSUMER_SECRET`: from the Twitter API
+- `ACCESS_TOKEN`: from the Twitter API
+- `ACCESS_TOKEN_SECRET`: from the Twitter API
+4. Run `node index.js` or `npm run start`
---
+++ index.js
@@ -0,0 +1,89 @@
+require('dotenv').config();
+const fs = require('fs'),
+    path = require('path')
+    Twit = require('twit'),
+    config = {
+    /* Be sure to update the .env file with your API keys. See how to get them: https://botwiki.org/tutorials/how-to-create-a-twitter-app */      
+      twitter: {
+        consumer_key: process.env.CONSUMER_KEY,
+        consumer_secret: process.env.CONSUMER_SECRET,
+        access_token: process.env.ACCESS_TOKEN,
+        access_token_secret: process.env.ACCESS_TOKEN_SECRET
+      },
+      mastodon: {
+        access_token: process.env.MASTODON_ACCESS_TOKEN,
+        api_url: process.env.MASTODON_API
+      }
+    },
+    T = new Twit(config.twitter),
+    Mastodon = require('mastodon');
+
+let M;
+try {
+  M = new Mastodon(config.mastodon);
+  console.log('ready to toot...');
+} catch(err) {
+  console.error(err);
+  console.error('please update your .env file')
+}
+
+const generateMessage = (fileName) => {
+  fileInfo = fileName.replace(/.(jpg)|(jpeg)|(png)/, '').replace(/-/g, ' ').replace(/(%\d?\d?[A-Z]?)/g, (match, $1) => {
+ return decodeURIComponent($1);
+}).split('_');
+  
+  return `${fileInfo[0]}, by ${fileInfo[1]}`;
+};
+
+fs.readdir(path.join(__dirname, 'images'), function (err, files) {
+  if (err) {
+    return console.log('Unable to scan directory: ' + err);
+  }
+
+  const randomImage = files[Math.floor(Math.random() * files.length)];
+  const message = generateMessage(randomImage);
+  const b64content = fs.readFileSync(`${__dirname}/images/${randomImage}`, { encoding: 'base64' });
+
+  M.post('media', { 
+    file: fs.createReadStream(`${__dirname}/images/${randomImage}`)
+  }, function (err, data, response) {
+    if (err){
+      console.log('ERROR:\n', err);
+    }
+    else {
+      console.log('tooting the image...');
+      M.post('statuses', {
+        status: message,
+        media_ids: new Array(data.id)
+      },
+      function(err, data, response) {
+        if (err){
+          console.log('ERROR:\n', err);
+        }
+        else{
+          console.log('tooted');
+        }
+      });
+    }
+  });
+  
+  T.post('media/upload', { media_data: b64content }, function(err, data, response) {
+    if (err){
+      console.log('error!', err);
+      res.sendStatus(500);
+    }
+    else{
+      console.log('tweeting the image...');
+      
+      T.post('statuses/update', {
+        status: message,
+        media_ids: new Array(data.media_id_string)
+      },
+      function(err, data, response) {
+        if (err){
+          console.log('ERROR:\n', err);
+        }
+      });
+    }
+  });
+});
